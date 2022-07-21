3e824d89ad793a3b5ebf368445748ad5073225d1
aquaform <aquaform@yandex.ru>
2022-07-21 19:08:17 +0500

    fix key problem

--- src/ItemList.js
+++ src/ItemList.js
@@ -4,20 +4,18 @@ import Item from "./Item"
 export default function ItemList(props) {
 	const {items, onDeleteItem} = props
 	return <ul className="ui-list">
-		<li className="ui-item-list">
 			{items.map((item) => {
 				return (
-					<>
-						<Item key={item.id} info={item} />
+					<li key={item.id} className="ui-item-list">
+						<Item info={item} />
 						<button
 							className="item-button"
 							onClick={() => onDeleteItem(item.id)}
 						>
 							Удалить
 						</button>
-					</>
+					</li>
 				);
 			})}
-		</li>
 	</ul>
 }
